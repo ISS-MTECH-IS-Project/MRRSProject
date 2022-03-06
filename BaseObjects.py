d1f54a2e0338a7048169a51314d34b46d40b5ed3
FatCatJL <yong_hui_jason@yahoo.com.sg>
2022-03-06 12:54:08 +0800

    BaseObjects revised with most objects mentioned in the matching flow

--- BaseObjects.py
+++ BaseObjects.py
@@ -8,50 +8,67 @@ class Topic:
     def __init__(self, name, tokens, test=False): 
         self.Name = name
         self.Tokens = tokens #[]
-        if self.test:
+        if test:
             print(self.Name)
             print(self.Tokens)
 
 class SymptomTopic(Topic):
-    def __init__(self, name, tokens, conditions, test=False): 
+    def __init__(self, name, tokens, conditions, questions=[], test=False): 
         super().__init__(self, name, tokens, test=False)
-        self.Conditions = conditions #an array of condition name strings
+        self.Conditions = conditions #an array of condition objects
+        self.Questions = questions #questions are an array of strings that are useful to verify this symptom
+        if test:
+            print(self.Conditions)
+            print(self.Questions)
 
 class ConditionTopic(Topic):
-    def __init__(self, name, tokens, symptoms, factsheet, test=False): 
+    def __init__(self, name, tokens, symptoms, factsheet, questions=[], test=False): 
         super().__init__(self, name, tokens, test=False)
         self.Symptoms = symptoms #an array of symptom name strings
+        self.Questions = questions #questions are an array of strings that are useful to verify this condition
         self.MedicalFactSheet = factsheet # imagine this like a "page of medical facts for the user to read on the details of this condition"
+        if test:
+            print(self.Symptoms)
+            print(self.Questions)
+            print(self.MedicalFactSheet)
 
 class CategoryTopic(Topic):
-    def __init__(self, name, tokens, symptoms, conditions, test=False): 
+    def __init__(self, name, tokens, symptoms, conditions, questions=[], test=False): 
         super().__init__(self, name, tokens, test=False)
-        self.Symptoms = symptoms #an array of symptom name strings
+        self.Symptoms = symptoms #an array of symptom objects
         self.Conditions = conditions #an array of condition name strings
+        self.Questions = questions #questions are an array of strings that are useful to verify this symptom
+        if test:
+            print(self.Symptoms)
+            print(self.Conditions)
+            print(self.Questions)
 
 
-# Scenario - BotUI Needs to ask question
+# Scenario - Chatbot needs to ask question, user will also ask question
 # Question class -  this will store
-# 1) Original question
+# 1) Questions by ChatBot
 # 2) UserInputs - these are inputs by user (be it probes / followup) 
 # 3) eg { orig : ['token of original question']}
 #       { d-treeID : ['above + tokens added after probe'] }
 #       { d-treeID2 : ['above + tokens added after probe'] }
 
 class Question:
-     def __init__(self, question, test=False): 
-        self.question = question
-        if self.test:
-            print(self.question)
+     def __init__(self, questionID, question, tokens=[], test=False): 
+        self.QuestionID = questionID # this is the ID used for indexing
+        self.Question = question # the question string
+        self.Tokens = tokens # we put the tokens here since this will be the object for iteration
+        if test:
+            print(self.Question)
 
-class UserQuestion(Question):
+# UserChat object is unique as it will consist of the original user question as well as storing a list of Question Objects
+class UserChat (Question):
     def __init__(self, question, test): 
         super().__init__(self, question, test)        
         self.UserInput = {}
 
-    def setStageToken(self, DtreeID, tokens):
-        if DtreeID not in self.UserInput.keys():
-            self.UserInput[DtreeID] = tokens
+    def addQuestionObject(self, QuestionObj):
+        if QuestionObj not in self.UserInput.keys():
+            self.UserInput[QuestionObj.QuestionID] = QuestionObj
         else:
             # this question is asked before (should not happen)
             pass
@@ -106,6 +123,9 @@ class MatchingAlgorithm:
         #       Condition 2 [white slime eyes]
         #       Condition 3 [green white slime]
         # Probe obj will be built with the following question tokens [spot, scale, white, eyes, green] <= note the last white is removed because it is repeated token
+        # We can have some RBS here to process the above such that we prioritize SYMPTOMS 
+        # => therefore token like this [spot, scale, white, eyes, green] will be sorted to
+        # [spot, white, green, scale, eyes] <= [SYMPTOM, SYMPTOM_descriptor, SYMPTOM_descriptor, BODYPART, BODYPART]
         pass
 
     # assume we are not probing because the n topics are very similiar, so we do FOLLOWUP (a kind of probe, but we single out differences of the tokens between the topics so that we can zoom in)
@@ -114,8 +134,41 @@ class MatchingAlgorithm:
     #       Condition 2 [white slime eyes]
     #       Condition 3 [green white slime]
     # eliminate all common tokens => we are left with [spot, scale, eyes, green] <= questions will be based on this 
+    # Apply RBS, we get sorted like [spot, green, scale, eyes] <= [SYMPTOM, SYMPTOM_descriptor, BODYPART, BODYPART]
     def builtFollowUpObject():
         pass
 
+
+
+# Scenario - using Topic matching, we decide to go ahead with PROBE
+# The probe object sent to the UI will have the following purpose
+# 1) Send back the original UserQuestion (this design is useful for single user session atm)
+# 2) When at Topic Matching stage, generate the list of tokens for question asking, and insert into this object (which will act as the iteration list for probing)
+# 3) Symptom matching to improve results OR getting more tokens (user inputs) - the focus is really not to be abductive
+
+class ProbeObject:
+    def __init__(self, name, userQuestionObj, test=False): 
+        self.Name = name # for session implementation if there's time
+        self.UserQuestion = userQuestionObj
+        self.ProbeQuestions = []
+    
+    def BakeQuestions (self, pre_sorted_question_array):
+        self.ProbeQuestions.append(pre_sorted_question_array)
+
+
+# Scenario - using Topic matching, we decide to go ahead with FOLLOWUP
+# The Followup object is a variant of the ProbeObject, but the design difference at this moment is to 
+# 1) Recognise this as a stage differential - meaning by right at this stage we no longer do probing - highly abductive and high inference
+#    (let's say if user exahust all follow up question with no matching - we will just return the HIGHEST MATCH condition / [conditions] - be it this might be low confidence)
+# 2) To facilitate the above, suspected conditions are also bake into this object
+
+class FollowUpObject (ProbeObject):
+    def __init__(self, name, userQuestionObj, suspected_conditions, test=False): 
+        super().__init__(self, name, userQuestionObj, test=False)
+        self.isProbe = False
+        self.Conditions = suspected_conditions
+    
+    def BakeConditions (self, pre_sorted_condition_array):
+        self.ProbeQuestions.append(pre_sorted_condition_array)
 
 
