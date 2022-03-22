2bafabe79c6ef92a2457840d6a6faffe4cab22be
Alan Wang <101057339+wangsong300@users.noreply.github.com>
2022-03-22 22:41:30 +0800

    conversation flow

--- SystemCode/conversation.py
+++ SystemCode/conversation.py
@@ -1,3 +1,4 @@
+from xmlrpc.client import boolean
 from models import *
 from topicmodel import *
 from kbdecision import *
@@ -24,7 +25,33 @@ class Conversation(metaclass=SingletonMeta):
         pass
 
 
-    def handleConversation(userInput:str) :
+    def handleConversation(self, caseId:str, userInput:str, isConfirmed:boolean, isGuided:boolean) -> dict: 
+        case = None
+        dbcon = DataAccessLayer().CreateDBConnection
+        if caseId:
+            case = dbcon.CreateOrGetCaseNode(name=caseId)
+        else:
+            case = dbcon.CreateOrGetCaseNode()
+        if isGuided:
+            inputSymptom = dbcon.GetOneSymptomNode(userInput)
+            nextAction = self.kb.getNext(case, inputSymptom, isConfirmed, 0, isGuided)
+        else:
+            supsepectedSym = self.tm.getSymptoms(userInput)
+            for (sym, level) in supsepectedSym:
+                nextAction = self.kb.getNext(case, sym, False, level, False)
+
+        nextSymptom = nextAction.symptom
+        diseases = nextAction.diseases
+        confirmeDisease = None
+        for d in diseases:
+            confidence = diseases[d]
+            if confidence>0.8:
+                confirmeDisease = d
+        
+        questionToAsk = nextSymptom.question
+
+        return {"case": case, "question":questionToAsk, "confirmed":confirmeDisease}
+
         # tm.getSymptom(userInput)
         # res = self.kb.getNext(case, sym)
         #  d = res.diseases[0]
@@ -32,4 +59,4 @@ class Conversation(metaclass=SingletonMeta):
         #     if d.confidency>0.9:
         #         return ConfirmDiseaseMessage(d)
         # return getMessage(res.symtom)
-        pass
+        
