d73924a91f8dbc396b1b7bbdac6771eb35b73784
Alan Wang <101057339+wangsong300@users.noreply.github.com>
2022-03-24 00:31:59 +0800

    kb decision maker

--- SystemCode/kbdecision.py
+++ SystemCode/kbdecision.py
@@ -4,27 +4,37 @@ from DataAccessLayer import DataAccessLayer
 from NeomodelGraphObject import *
 class KbDecisionMaker(metaclass=SingletonMeta):
 
-
-    def __init__(self) -> None:
-        pass
-
-
-    def getNext(self, case:CaseInc, symptom:Symptom, isConfirmed:boolean, suspectLevel:float, isGuided:boolean) -> dict :
+    def getNext(self, case:CaseInc, symptom:Symptom, isConfirmed:boolean, suspectLevel:float, isGuided:boolean) :
         """
         return a dict
         {"symptom": next Symtom if any
         "diseases": [{"disease": disease, "confidence":0.8}]
         list of dictionary of diseases and confidence level sorted by confidence level
         """
-        dbcon = DataAccessLayer().CreateDBConnection
+        dbcon = DataAccessLayer(username='neo4j',password='ai-user').CreateDBConnection
         currentDiseases = dbcon.GetAllDiseaseFromSymptomOrCaseByNameOrID(case.name)
         suspectDiseases = dbcon.GetAllDiseaseFromSymptomOrCaseByNameOrID(symptom.name)
-        for d in suspectDiseases:
-            if currentDiseases[d]:
-                disease = currentDiseases[d]
-                # logic here
+        currentDMap = { currentDiseases[i][0].id : currentDiseases[i][0] for i in range(len(currentDiseases))} if currentDiseases[0][0] else {}
+        for dl in suspectDiseases:
+            d = dl[0]
+            weight = d.symptoms.relationship(symptom).weight
+            if isGuided:
+                if isConfirmed:
+                    suspectLevel = weight
+                else:
+                    suspectLevel = d.symptoms.relationship(symptom).penalty
+            diff = suspectLevel
+            newConfidence = suspectLevel/len(d.symptoms)
+            if d.id in currentDMap:
+                rel = case.suspected_symptoms.relationship(symptom)
+                if rel:
+                    diff = suspectLevel - rel.suspectedLevel
+                rel = case.suspected_diseases.relationship(d)
+                newConfidence = rel.confidence + (diff*weight)/len(d.symptoms)
+            case = dbcon.UpdateDiseaseToCase(case, d, newConfidence)
+        case = dbcon.UpdateSymptomToCase(case, symptom, suspectLevel)
 
-        pass
+        
 
 
 
