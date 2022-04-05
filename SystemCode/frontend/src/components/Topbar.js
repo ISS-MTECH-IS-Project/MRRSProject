6a3c0f0c257eab9178f2e2345e22054d9d06f5ce
FatCatJL <yong_hui_jason@yahoo.com.sg>
2022-04-05 15:06:30 +0800

    small UI Change

--- SystemCode/frontend/src/components/Topbar.js
+++ SystemCode/frontend/src/components/Topbar.js
@@ -1,31 +1,15 @@
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import { Box } from "@mui/material";
+import Grid from "@mui/material/Grid";
+
 const Topbar = () => {
   return (
-    <div className="lc-topbar-main">
-      <div id="top-bar" className="lc-topbar">
-        <div className="lc-topbar-seprator"></div>
-        <div className="lc-topbar-l1">
-          <div className="lc-topbar-l2">
-            <div className="lc-topbar-l3">
-              <div className="lc-topbar-left">
-                <div className="lc-topbar-col">
-                  <img
-                    alt="avatar"
-                    src="https://cdn.livechat-files.com/api/file/lc/img/13750077/fa899ea283029a5eaec0cc5cbeae30c9.png"
-                    className="lc-topbar-icon"
-                  />
-                </div>
-                <div className="lc-online"></div>
-              </div>
-              <div className="lc-topbar-right">
-                <div className="lc-topbar-titlebox">
-                  <div className="lc-topbar-title">ChatBot</div>
-                  <div className="lc-topbar-subtitle">Support Agent</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div id="title" className="titlebar">
+      <Grid container direction="row" alignItems="center">
+        <SmartToyIcon />
+        <Box m={2} pt={3}></Box>
+        OhMyFishBot is here to serve
+      </Grid>
     </div>
   );
 };
