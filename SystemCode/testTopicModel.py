9ae13e5a3b2a2115d2dea3937e38f613499e8782
siopoh <siopoh@gmail.com>
2022-04-08 00:09:31 +0800

    Updates to topicmodel with stopwords
    - Added stopwords processing
    - Improve results

--- SystemCode/testTopicModel.py
+++ SystemCode/testTopicModel.py
@@ -1,5 +1,6 @@
 from topicmodel import TopicModel
 
+<<<<<<< Updated upstream
 question = "My fish has worm sticking out from its anus."
 #question = "My fish eye so bloody out and the scale is coming out. Yesterday night, he does not eat and is not balancing well OH THE EYES now like cloud"
 tm = TopicModel()
@@ -11,5 +12,29 @@ for s in symptoms:
     for d in s.diseases:
         print('disease: ', d.name)
     print()
+=======
+dbcon = DataAccessLayer(username='neo4j',password='password').CreateDBConnection
+#question = "My fish has worm sticking out from its anus."
+#question = "boy My fish has sore anus"
+#question = "my fish has worm on body"
+question = "My fish eye so bloody out and the scale is coming out. Yesterday night, he does not eat and is not balancing well OH THE EYES now like cloud"
+#question = "My fish eye so bloody out and the scale is coming out."
+#question = "Yesterday night, my fish does not eat and is not balancing well"
+tm = TopicModel()
 
-print(tm.convert_to_tokens("Grey or green dots on skin"))
+symptoms = tm.getSymptoms(dbcon, question, verbose=True)
+#print(symptoms)
+if len(symptoms) > 0:    
+    for key in symptoms:
+        # print('*** symptom ***: ', s.description)
+        # print('** confidence **: ', s.confidence)
+        # print('* question *: ', s.question)
+        mysymp = dbcon.GetOneSymptomNode(key)
+        print(key)
+        for d in mysymp.diseases:
+            print('disease: ', d.name)
+else:
+    print('No disease found')
+>>>>>>> Stashed changes
+
+#print(tm.convert_to_tokens("Grey or green dots on skin"))
