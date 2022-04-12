e925fcc0c147ce7781b2a3f73629c271dd946e64
siopoh <siopoh@gmail.com>
2022-04-12 23:44:37 +0800

    Updates to TopicModel and stopwords.txt
    - Improve performance for TopicModel
    - Update stopwords

--- SystemCode/testTopicModel.py
+++ SystemCode/testTopicModel.py
@@ -1,13 +1,17 @@
 from topicmodel import TopicModel
 from DataAccessLayer import DataAccessLayer
 
-dbcon = DataAccessLayer(username='neo4j',password='neo123456').CreateDBConnection
-#question = "My fish has worm sticking out from its anus."
+dbcon = DataAccessLayer(username='neo4j',password='password').CreateDBConnection
+question = "My fish has worm sticking out from its anus."
 #question = "boy My fish has sore anus"
 #question = "my fish has worm on body"
-question = "My fish eye so bloody out and the scale is coming out. Yesterday night, he does not eat and is not balancing well OH THE EYES now like cloud"
+#question = "My fish eye so bloody out and the scale is coming out. Yesterday night, he does not eat and is not balancing well OH THE EYES now like cloud"
 #question = "My fish eye so bloody out and the scale is coming out."
 #question = "Yesterday night, my fish does not eat and is not balancing well"
+#question = "White spot"
+#question = "My fish is is dying you know it's dying you got to save my fish, it just flipped and the nigga is banging the wall"
+#question = "worm in anus"
+
 tm = TopicModel()
 
 symptoms = tm.getSymptoms(dbcon, question, verbose=True)
